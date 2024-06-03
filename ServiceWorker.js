4275021c003e46571d78e2d04ea4118cf2a96dd0
Tamzid Choudhury <74117270+zzztamzzz@users.noreply.github.com>
2024-06-03 18:04:41 -0400

    initial

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-El_Pollo_Loco_Collab-1.0";
+const contentToCache = [
+    "Build/r2.loader.js",
+    "Build/r2.framework.js.unityweb",
+    "Build/r2.data.unityweb",
+    "Build/r2.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
